573e484cd898e3d4498597c658cc6ee7cc3ce3d4
Yeison Montoya <yeisondiseno@gmail.com>
2020-09-02 16:06:25 -0500

    Y.Montoya: promise, mejores que los callbacks

---
+++ src/promise/challenge.js
@@ -0,0 +1,18 @@
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const API = 'https://rickandmortyapi.com/api/character/';
+
+function fetchData( url_api, callback ) {
+    let xhttp = new XMLHttpRequest(); 
+    xhttp.open('GET', url_api, true); // terecer parametro activa el asincronismo
+    xhttp.onreadystatechange = function ( event ) {
+        if(xhttp.readyState === 4){
+            if( xhttp.status === 200 ) {
+                callback(null, JSON.parse(xhttp.responseText));
+            } else {
+                const error = new Error('Error' + url_api);
+                return callback(error, null);
+            }
+        }
+    }
+    xhttp.send();
+}
